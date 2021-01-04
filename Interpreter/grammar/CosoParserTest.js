87b3ad12ffe4919de02c2c9ba7d33f8d8dc6d8da
erez blumenstock <erez.blumenstock@gmail.com>
2021-01-04 21:11:44 +0200

    grammar: test load from file

--- Interpreter/grammar/CosoParserTest.js
+++ Interpreter/grammar/CosoParserTest.js
@@ -1,12 +1,16 @@
 import antlr4 from 'antlr4';
 import CosoLexer from './CosoLexer.js'
 import CosoParser from './CosoParser.js'
+import fs from 'node:fs';
 
-var input = "erez\n  aa\n    bb val\n\n";
-var chars = new antlr4.InputStream(input);
-var lexer = new CosoLexer(chars);
-var tokens  = new antlr4.CommonTokenStream(lexer);
-var parser = new CosoParser(tokens);
-parser.buildParseTrees = true;   
-var tree = parser.main();
-console.log(tree.toStringTree());
+fs.readFile('test.coso', 'utf8', function(err, input) {
+    if (err) throw err;
+    var chars = new antlr4.InputStream(input);
+    var lexer = new CosoLexer(chars);
+    var tokens  = new antlr4.CommonTokenStream(lexer);
+    var parser = new CosoParser(tokens);
+    parser.buildParseTrees = true;   
+    var tree = parser.main();
+    console.log(tree.toStringTree());
+});
+
